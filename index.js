b2eaff28ed47c8ca2faa40ff4cf29e67ba546a12
Kadson Lima <kadson-kaio@hotmail.com>
2022-09-03 15:01:37 -0300

    feat: placar

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function teste() {
+	// body...
+	const tabela = document.querySelector(".tabelas");
+	const confronto = document.querySelector(".confrontos");
+
+	tabela.classList.toggle("hidden")
+	confronto.classList.toggle("hidden")
+}
